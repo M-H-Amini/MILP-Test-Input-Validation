57bc6c3f865e5e2a23023b489c45bd88ea017c83
Alina SH <157328243+MistyHaven@users.noreply.github.com>
2025-05-20 19:43:14 -0400

    Update csv_reader.py
    Edit to code => assumption was wrong about original and synthesized pairs, so had to re-do it. Old code and notes are commented out.

--- csv_reader.py
+++ csv_reader.py
@@ -3,18 +3,80 @@ from PIL import Image
 from pandas import *
 import requests
 from io import BytesIO
+from collections import defaultdict
+
+
+def download_img(url):
+  img = requests.get(url)
+  return Image.open(BytesIO(img.content))
+  """
+  except Exception as e:
+        print(f"Failed to load image: {url} — {e}")
+        return None
+  
+  """
+
+
+def id_extracter(url):
+  name = url.split("/")[-1]
+  img_id = name.split("_",1)[0] #separates at last underscore
+  return img_id
+
+
 
 #for any csv
 def csv_reader(csv_path, chosen_label):
   df = read_csv(csv_path)
+
+  # new column for image ids
+  df['img_id'] = df['image_link'].apply(id_extracter)
+
   #assuming that will be the column title for the links
-  links = df['image_link'].tolist()
-  total = len(links)
+  #links = df['image_link'].tolist()
+  #total = len(links)
   #assuming that will be the column title for the label
-  valid = df['ground_truth'].tolist()
+  #valid = df['ground_truth'].tolist()
   pairs = []
   
-  for i in range(0, total-1,2):
+
+  #Image Pairs:
+
+  for img_id, group in df.groupby('img_id'):
+    origin = group[group["transform"] == "original"]
+    if len(origin) == 0:
+        # Skip if no original images
+        continue  
+
+    #get the og img and download it
+    origin_row = origin.iloc[0]
+    img1 = download_img(origin_row["image_link"])
+
+    for _, row in group.iterrows():
+        if row["transform"] == "original":
+            # skip original => we already have it
+            continue  
+        
+        img2 = download_img(row["image_link"])
+        validLabel = row["ground_truth"]
+        if validLabel==chosen_label:
+          validLabel = 1
+        else:
+           validLabel = 0
+
+        # do we need an IQA label as well?
+       
+        item = {'img1':img1, 'img2':img2, 'label':validLabel}
+        pairs.append(item)
+        #item = (img1, img2, label)
+        #pairs.append((img1, img2, label))
+  
+  print(f"Loaded {len(pairs)} image pairs.")
+  return pairs
+
+
+
+
+  """for i in range(0, total-1,2):
    #image pairs
     url1 = links[i]
     img1 = Image.open(BytesIO(requests.get(url1).content))
@@ -44,15 +106,10 @@ def csv_reader(csv_path, chosen_label):
 
 cifar10 = csv_reader('csv_files/cifar10_experiment_results.csv','car')
 imagenet = csv_reader('csv_files/imagenet_experiment_results.csv','car')
-print(cifar10)
-
-
-
-
-
-# here is the old code, before it became a general function
+print(cifar10)"""
 
 """
+
 #read the csv files
 csvFile1 = read_csv('csv_files/cifar10_experiment_results.csv')
 csvFile2 = read_csv('csv_files/imagenet_experiment_results.csv')
@@ -135,6 +192,7 @@ for i in range(0,total2-1,2):
   
 #return for each (img1, img2, label) 
 #or ('img1': img1, 'img2' : img2, 'label' : label)
+
 
 """
 
