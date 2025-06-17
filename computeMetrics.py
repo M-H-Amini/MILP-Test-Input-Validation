d041216399816c77242b6dc5fe8cdf5d9cfd73bf
Alina SH <157328243+MistyHaven@users.noreply.github.com>
2025-06-17 11:48:38 -0400

    Update computeMetrics.py
    Training was extremely slow before so I changed some things here. Now, it's processing a lot quicker. Turns out I was recalling the VGG16 model and others with each pair (pretty heavy).

--- computeMetrics.py
+++ computeMetrics.py
@@ -47,6 +47,13 @@ from torchvision.transforms.functional import to_pil_image
 import torch
 
 
+# Global model definitions (load only once)
+vgg_model = VGG16(include_top=False, input_shape=(224, 224, 3))
+fcn_weights = FCN_ResNet50_Weights.DEFAULT
+fcn_model = fcn_resnet50(weights=fcn_weights)
+fcn_model.eval()
+
+
 
 def computeMetrics(img_A, img_B):
   """_summary_
@@ -61,8 +68,10 @@ def computeMetrics(img_A, img_B):
   input_shape = (224,224,3)
 
   #resizing our images:
-  img_A_new = cv2.resize(img_A,input_shape[:2]) #assuming (32,32) is the resolution we want
-  img_B_new = cv2.resize(img_B,input_shape[:2])
+  #img_A_new = cv2.resize(img_A,input_shape[:2]) #assuming (32,32) is the resolution we want
+  img_A_new = cv2.resize(img_A, (input_shape[1], input_shape[0]))
+  img_B_new = cv2.resize(img_B, (input_shape[1], input_shape[0]))
+  #img_B_new = cv2.resize(img_B,input_shape[:2])
   # Check if images loaded successfully
   if img_A_new is None or img_B_new is None:
     print("Error: One or both images could not be loaded. Check paths.")
@@ -72,7 +81,10 @@ def computeMetrics(img_A, img_B):
   
 
 #CS
-  features = model = VGG16(include_top=False, input_shape =input_shape)
+  features = vgg_model
+  model = fcn_model
+
+  #features = model = VGG16(include_top=False, input_shape =input_shape)
   #Only extracts mid to low level features (no deep ones)
   # features = model(inputs=model.input, outputs=model.get_layer("block2_conv2").output)
   
@@ -137,13 +149,14 @@ def computeMetrics(img_A, img_B):
   rgb_A_float = img_as_float(rgb_A)
   rgb_B_float = img_as_float(rgb_B)
   ssim_result = structural_similarity(rgb_A_float, rgb_B_float, data_range=1.0, channel_axis=-1)
+  #rgb_A_float = np.clip(rgb_A.astype(np.float32) / 255.0, 0.0, 1.0)
   #where last axis is the colours channel
 
 
 #sss
   weights = FCN_ResNet50_Weights.DEFAULT
-  model = fcn_resnet50(weights=weights)
-  model.eval()
+  #model = fcn_resnet50(weights=weights)
+  #model.eval()
 
   #normalizes, resizes and converts images
   '''tensor_A = weights.transforms()(read_image(img_A, mode=ImageReadMode.RGB)).unsqueeze(0)
@@ -179,13 +192,13 @@ def computeMetrics(img_A, img_B):
   #assuming that distances = [5] (5px apart) and angles = [0] (horizontal to each other)
   glcm_A = compute_glcm_features(img_A_new, distances=distances, angles=angles) 
   glcm_B = compute_glcm_features(img_B_new, distances=distances, angles=angles) 
-  print((glcm_A))
+  #print((glcm_A))
 
   contr_A = glcm_A[0][0,0]
-  contr_B = glcm_A[0][0,0]
+  contr_B = glcm_B[0][0,0]
 
   dissim_A = glcm_A[1][0,0]
-  dissim_B = glcm_A[1][0,0]
+  dissim_B = glcm_B[1][0,0]
 
   glcm_contrast = abs(contr_A-contr_B)
   glcm_dissim = abs(dissim_A-dissim_B)
@@ -198,9 +211,9 @@ def computeMetrics(img_A, img_B):
 #wd
   wd_result = wasserstein_distance(g_A.flatten(), g_B.flatten())
 
-
+  metric_names = ["CPL", "CS", "KL", "MSE", "HistCorr", "HistInter", "PSNR", "SSIM", "SSS", "TSI", "WD"]
   metrics = np.array([cpl_result, cs_result, kl_result, mse_result, hist_corr, hist_inter,psnr_result,ssim_result, sss_result, tsi_result, wd_result])
-  return metrics
+  return metric_names, metrics
 
 def my_mse(img1, img2):
     h, w, c = img1.shape
@@ -219,319 +232,3 @@ def compute_glcm_features(image, distances, angles):
     correlation = graycoprops(glcm, prop='correlation')
     metrics = (contrast, dissimilarity)
     return metrics
-
-'''
-if __name__ == "__main__":
-  """ _description_
-  
-  Sets your CV file in order to extract your two images.
-  
-  Then, assigns img_A and img_B to an image respectively, 
-  then computes the associated metrics using the computeMetrics function.
-  Although you would need to extract these two images from a csv file,
-  for the sake of demonstrating how the function computeMetrics works,
-  we have provided a ready-made sample-pair for you to use.
-
-  You would need to use these lines for the images if you were to use it:
-    img_A = cv2.imread(" path of image ")
-    img_B = cv2.imread(" path of image ") 
-     
-  """
-
-  csv_file = "imagenet_experiment_results.csv"
-  df = pd.read_csv(csv_file)
-  
-  image_pathA = "sampleImages/n02009912_5558.jpg" 
-  img_A = Image.open(image_pathA)
-  #img_A.show()
-
-  image_pathB = "sampleImages/n02009912_14750.JPEG.gaussian_noise_932.png"  # Replace with the actual path to your image
-  img_B = Image.open(image_pathB)
-  #img_B.show()
-  
-  metrics = computeMetrics(img_A, img_B)
-  #[cpl_result, cs_result, kl_result, mse_result, hist_corr, hist_inter,psnr_result,ssim_result, sss_result, tsi_result, wd_result]
-  print("The Computed Metrics for Image A and Image B: "+
-        "CPL: "+str(metrics[0])
-        +", CS: " + str(metrics[1])
-        +", KL: " + str(metrics[2])
-        +", MSE: " + str(metrics[3])
-        +", HIST_CORR: " + str(metrics[4])
-        +", HIST_INTER: " + str(metrics[5])
-        +", PSNR: " + str(metrics[6])
-        +", SSIM: " + str(metrics[7])
-        +", SSS: " + str(metrics[8])
-        +", TSI: " + str(metrics[9])
-        +", WD: " + str(metrics[10]))'''
-
-
-"""
-
-
-
-
-
-#####################################################
-
-NOTE:
-The code for majority of these metrics can be found in ICSE2025Industry.
-
-
-#####################################################
-
-
-def cpl(csv_file,df):
-    dataset_length = len(df)
-    img_org, img_gen = [], []
-    img_shape = (32, 32) 
-    for i in tqdm(range(dataset_length)):
-      img_org_ = cv2.resize(cv2.imread(df["original"][i]), img_shape).astype(float)
-      img_org.append(img_org_)
-      img_gen_ = cv2.resize(cv2.imread(df["gen"][i]), img_shape).astype(float)
-      img_gen.append(img_gen_)
-    
-    img_org = np.array(img_org)
-    img_gen = np.array(img_gen)
-    print(img_org.shape, img_gen.shape)
-    
-    # Model:
-    model = VGG16(weights='imagenet', include_top=False, input_shape=(*img_shape[::-1], 3))
-
-    X_org = preprocess_input(img_org)
-    X_gen = preprocess_input(img_gen)
-    X_org = model.predict(X_org)
-    X_gen = model.predict(X_gen)
-
-    X_org = X_org.reshape(len(X_org), -1)
-    X_gen = X_gen.reshape(len(X_gen), -1)
-
-    losses = np.array(mse(X_org, X_gen))
-
-    df["CPL"] = losses
-      #got rid of the print statement for this and the others.
-    df.to_csv(csv_file)
-
-
-# Cosine Similarity
-def cs(csv_file,df):
-  dataset_length = len(df)
-  img_org, img_gen = [], []
-  img_shape = (32, 32) 
-  for i in tqdm(range(dataset_length)):
-    img_org_ = cv2.resize(cv2.imread(df["original"][i]), img_shape).astype(float)
-    img_org.append(img_org_)
-    img_gen_ = cv2.resize(cv2.imread(df["gen"][i]), img_shape).astype(float)
-    img_gen.append(img_gen_)
-  
-  img_org = np.array(img_org)
-  img_gen = np.array(img_gen)
-  print(img_org.shape, img_gen.shape)
-  
-  #  Model:
-  model = VGG16(weights='imagenet', include_top=False, input_shape=(*img_shape[::-1], 3))
-  
-  X_org = preprocess_input(img_org)
-  X_gen = preprocess_input(img_gen)
-  X_org = model.predict(X_org)
-  X_gen = model.predict(X_gen)
-
-  X_org = X_org.reshape(len(X_org), -1)
-  X_gen = X_gen.reshape(len(X_gen), -1)
-
-  cs = cosine_similarity(X_org, X_gen)
-  cs = [cs[i,i] for i in range(len(cs))]
-
-  df["CS"] = cs
-  df.to_csv(csv_file)
-
-# Histogram comparison using correlation and intersection
-def hist_cmp(csv_file,df):
-  
-  dataset_length = df.shape[0]
-  hist_org = []
-  hist_gen = []
-  correlation = []
-  intersection = []
-  for i in tqdm(range(dataset_length)):
-    img_org = cv2.imread(df["original"][i], cv2.IMREAD_GRAYSCALE)
-    hist = cv2.calcHist([img_org], [0], None, [256], [0, 256])
-    hist_org.append(hist)
-    img_gen = cv2.imread(df["gen"][i], cv2.IMREAD_GRAYSCALE)
-    hist = cv2.calcHist([img_gen], [0], None, [256], [0, 256])
-    hist_gen.append(hist)
-    correlation.append(cv2.compareHist(hist_gen[i], hist_org[i], cv2.HISTCMP_CORREL))
-    intersection.append(cv2.compareHist(hist_gen[i], hist_org[i], cv2.HISTCMP_INTERSECT))
-  
-    
-  df["Hist_cor"] = correlation
-  df["Hist_int"] = intersection
-  df.to_csv(csv_file)
-  
-# Plot func from ICSE2025Industry => used in kl(csv_file,df)
-def rgb2gray(rgb):
-    return np.dot(rgb[...,:3], [0.299, 0.587, 0.144])
-
-
-# Kullback-Leibler divergence between image histograms 
-def kl(csv_file,df):
-  dataset_length = df.shape[0]
-  kl = []
-  for i in tqdm(range(dataset_length)):
-    img_org = rgb2gray(plt.imread(df["original"][i]))
-    out_org = plt.hist(x=img_org.ravel(), bins=256, range=[0, 256])
-    dist_org = out_org[0]/(img_org.shape[0]*img_org.shape[1])
-    img_gen = rgb2gray(plt.imread(df["gen"][i]))
-    out_gen = plt.hist(x=img_gen.ravel(), bins=256, range=[0, 256])
-    dist_gen = out_gen[0]/(img_gen.shape[0]*img_gen.shape[1])
-    kl.append(sum(rel_entr(dist_org, dist_gen)))
-  df["KL"] = kl
-  df.to_csv(csv_file)
-
-# Function from ICSE2025Industry => used in mse(csv_file,df)
-def my_mse(img1, img2):
-    h, w, c = img1.shape
-    err = np.sum(cv2.subtract(img1, img2)**2)
-    return err/float(w*h*c)
-
-
-def mse(csv_file,df):
-
-  dataset_length = df.shape[0]
-  mse = []
-  for i in tqdm(range(dataset_length)):
-    img_org = cv2.resize(cv2.imread(df["original"][i]), img_gen) 
-    img_gen = cv2.resize(cv2.imread(df["gen"][i]), img_gen) 
-    mse.append(my_mse(img_org, img_gen))
-
-  df["MSE"] = mse
-  df.to_csv(csv_file, index = False)
-
-#Peak signal-to-noise ratio
-def psnr(csv_file,df):
-  dataset_length = df.shape[0]
-  psnr = []
-  for i in tqdm(range(dataset_length)):
-    img_org = cv2.imread(df["original"][i])
-    img_gen = cv2.imread(df["gen"][i])
-    psnr.append(cv2.PSNR(img_org, img_gen))
-  df["PSNR"] = psnr
-  df.to_csv(csv_file)
-
-def ssim(csv_file,df):
-  dataset_length = df.shape[0]
-  ssim = []
-  for i in tqdm(range(dataset_length)):
-    img_org = img_as_float(cv2.imread(df["original"][i], cv2.IMREAD_GRAYSCALE))
-    img_gen = img_as_float(cv2.imread(df["gen"][i], cv2.IMREAD_GRAYSCALE))
-    ssim.append(structural_similarity (img_org, img_gen, data_range=img_gen.max()-img_gen.min()))
-  df["SSIM"] = ssim
-  df.to_csv(csv_file)
-
-def sss(csv_file,df):
-  #1: Mode initializes with best available weights
-  weights = FCN_ResNet50_Weights.DEFAULT
-  model = fcn_resnet50(weights=weights)
-  model.eval()
-  #2: Inference transforms are initialized
-  preprocess = weights.transforms()
-  dataset_length = df.shape[0]
-  sss = []
-
-  for i in tqdm(range(dataset_length)):
-    img_org = read_image(df["original"][i]) 
-    img_gen = read_image(df["gen"][i])
-
-    #3: Inference preprocessing transforms are applied
-    batch_org = preprocess(img_org).unsqueeze(0)
-    batch_gen = preprocess(img_gen).unsqueeze(0)
-
-    #4: Use the model and visualize the prediction
-    prediction_org = model(batch_org)["out"]
-    normalized_masks_org = prediction_org.softmax(dim=1)[0, 0]
-    prediction_gen = model(batch_gen)["out"]
-    normalized_masks_gen = prediction_gen.softmax(dim=1)[0, 0]
-    gen = normalized_masks_gen.detach().numpy() 
-    org = normalized_masks_org.detach().numpy() 
-    loss = np.square(np.subtract(org, gen)).mean() 
-    sss.append(loss)
-  
-  df["SSS"] = sss
-  df.to_csv(csv_file)
-
-# Function from ICSE2025Industry => used in tsi(csv_file,df)
-def compute_glcm_features(image, distances, angles):
- # Compute GLCM
-    gray_image = img_as_ubyte(color.rgb2gray(image))
-    glcm = graycomatrix(gray_image, distances=distances, angles=angles, levels=256, symmetric=True, normed=True)
-    # GLCM properties computed
-    contrast = graycoprops(glcm, prop='contrast')
-    dissimilarity = graycoprops(glcm, prop='dissimilarity')
-    homogeneity = graycoprops(glcm, prop='homogeneity')
-    energy = graycoprops(glcm, prop='energy')
-    correlation = graycoprops(glcm, prop='correlation')
-    metrics = (dissimilarity,)
-    return metrics
-
-# Texture similarity using gray level co-occurrence matrix
-def tsi(csv_file,df):
-  img_shape = (512, 320) 
-  dataset_length = len(df)
-  img_org, img_gen = [], []
-  similarity = []
-
-  for i in tqdm(range(dataset_length)):
-    img_org_ = cv2.resize(cv2.imread(df["original"][i]), img_shape)
-    img_org.append(img_org_)
-    img_gen_ = cv2.resize(cv2.imread(df["gen"][i]), img_shape)
-    img_gen.append(img_gen_)
-    f1 = compute_glcm_features(img_org_, distances=[1], angles=[0])
-    f2 = compute_glcm_features(img_gen_, distances=[1], angles=[0])
-    similarity.append(np.linalg.norm(np.array(f1) - np.array(f2)))
-    
-  img_org = np.array(img_org)
-  img_gen = np.array(img_gen)
-  print(img_org.shape, img_gen.shape)
-  
-  df["TSI"] = similarity
-  df.to_csv(csv_file, index=False)
-
-
-# Wasserstein distance between grayscale images
-def wd(csv_file,df):
-  dataset_length = df.shape[0]
-  wd = []
-  for i in tqdm(range(dataset_length)):
-    img_org = cv2.imread(df["original"][i], cv2.IMREAD_GRAYSCALE).flatten()
-    img_gen = cv2.imread(df["gen"][i], cv2.IMREAD_GRAYSCALE).flatten()
-    wd.append(wasserstein_distance(img_org, img_gen))
-  df["WD"] = wd
-  df.to_csv(csv_file)
-  
-
-# Variance Inflation Factor for feature multicollinearity
-def vif(csv_file):
-  df = pd.read_csv(csv_file)
-  num_df = df.select_dtypes(include=[float, int])
- 
-  vif = pd.DataFrame()
-  vif["feature"] = num_df.columns
-  vif["VIF"] = [variance_inflation_factor(num_df.values, i) for i in range(num_df.shape[1])]
-
-  vif.to_csv("vif_results.vsf", index=False)
-  #should we return vif?
-
-"""
-
-
-
-    
-
-
-  
-
-
-
-
-
-
-
