c6a8899581084cc2e59906337770620ad3ec59f2
Alina SH <157328243+MistyHaven@users.noreply.github.com>
2025-06-23 14:56:41 -0400

    Update classifier_training.py
    Code fixed, works and trains properly now.

--- classifier_training.py
+++ classifier_training.py
@@ -7,7 +7,7 @@ from sklearn import tree
 #Random Forest
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, recall_score, ConfusionMatrixDisplay
-#from sklearn.model_selection import RandomizedSearchCV, train_test_split
+#from sklearn.model_selection import RandomizedSearchCV, train_test_split 
 #from scipy.stats import randint
 
 #Logistic Regression
@@ -17,9 +17,9 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier 
 
 # Import functions from other files
-#from csv_reader import *
-from csv_reader_cifar10 import *
+from csv_reader import *
 #from csv_reader_cifar10 import *
+
 
 #SVM with kernel (try for all three)
 #from sklearn import svm
@@ -29,7 +29,9 @@ from sklearn.svm import SVC
 #Predictions:
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
-def classifier_modeler(int_choice, ds_train, ds_test): #classifier_choice
+
+
+def classifier_modeler(int_choice, kernel_choice, ds_train, ds_test): #classifier_choice
     
     #classifiers: 0 - SVM, 1 - Logistic Regression, 2 - Decision Tree, 3 - Random Forest
 
@@ -37,42 +39,42 @@ def classifier_modeler(int_choice, ds_train, ds_test): #classifier_choice
     X_test, y_test = compute_metrics_on_dataset(ds_test, ds_folder='csv_images')"""
 
     print("Preprocessing training data...")
-    X_train, y_train = compute_metrics_on_dataset(ds_train, ds_folder='cifar_10_images')
+    X_train, y_train = compute_metrics_on_dataset(ds_train, ds_folder='csv_images')
     print("Preprocessing on training done!")
 
     print("Preprocessing testing data...")
-    X_test, y_test = compute_metrics_on_dataset(ds_test, ds_folder='cifar_10_images')
-    input("Preprocessing on testing done!")
+    X_test, y_test = compute_metrics_on_dataset(ds_test, ds_folder='csv_images') #cifar_10_images
+    print("Preprocessing on testing done!")
 
-    model = model_training(int_choice, X_train, y_train)
+    model = model_training(int_choice, kernel_choice, X_train, y_train)
     if model is None:
         print("Model training failed.")
         return
     prediction_report(model, X_train, y_train,X_test, y_test)
 
-def model_training(choice, X_train, y_train):
-    print("MEOW MEOW MEOW", choice)
+def model_training(choice, kernel_choice, X_train, y_train):
     if choice == 0:
         
-        while True:
+        """  while True:
             try: 
+                print("About to ask for kernel choice...")
                 kernel_choice = int(input("Specify your desired kernel: 0 - RBF, 1 - Linear, 2 - Poly "))
                 if kernel_choice in [0,1,2]:
                     break
             except ValueError:
-                print("Please enter a valid integer (0, 1, or 2).")
+                print("Please enter a valid integer (0, 1, or 2).")"""
         if kernel_choice == 0:
             # rbf:W
             #Train:
             model = SVC(kernel="rbf", gamma=0.5, C=1.0)
             kernel_name = "RBF Kernel"
+            
         
         elif kernel_choice == 1:
         # linear
             #Train:
             model = SVC(kernel="linear", C=1.0)
             kernel_name = "Linear Kernel"
-
         elif kernel_choice == 2:
             # poly
              #Train:
@@ -129,10 +131,10 @@ def prediction_report(model, X_train, y_train,X_test, y_test):
 if __name__ == '__main__':
 
   #You can use a different csv file. This is just a sample
-  """csv_file_path = "imagenet_experiment_results.csv" 
-  folder_name = "csv_images" """
-  csv_file_path = "cifar10_experiment_results.csv" 
-  folder_name = "cifar_10_images"
+  csv_file_path = "imagenet_experiment_results.csv" 
+  folder_name = "csv_images" 
+  #csv_file_path = "cifar10_experiment_results.csv" 
+  #folder_name = "cifar_10_images"
   img_url_col = "image_link"
   percent = 0.2
   
@@ -153,6 +155,7 @@ if __name__ == '__main__':
     # specify what classifier you want
   print("classifiers: 0 - SVM, 1 - Logistic Regression, 2 - Decision Tree, 3 - Random Forest")
   looper = True
+  kernel_choice = 0
   while looper == True:
         try:
             classifier_choice = int(input("Specify your choice of classifier (type -1 to exit):").strip())
@@ -160,7 +163,17 @@ if __name__ == '__main__':
                 looper = False
             elif classifier_choice == 0 or classifier_choice == 1 or classifier_choice == 2 or classifier_choice == 3 :
                 looper = False
-                classifier_modeler(classifier_choice, train_dataset, test_dataset)
+                if classifier_choice == 0:
+                    while True:
+                        try:
+                            print("Specify your desired kernel: 0 - RBF, 1 - Linear, 2 - Poly ")
+                            kernel_choice = int(input("Specify your choice of kernel (type -1 to exit):").strip())
+                            if classifier_choice == -1:
+                                break
+                            break
+                        except ValueError:
+                            print("Please input one of the specified integers")
+                classifier_modeler(classifier_choice, kernel_choice, train_dataset, test_dataset)
             else:
                 print("Please choose one of the specified values!")
         except ValueError:
@@ -168,6 +181,5 @@ if __name__ == '__main__':
 
         #except ValueError:
          #   print("Please enter a valid integer.")
-
 
 
