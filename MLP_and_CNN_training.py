c3f2323e98d7243be12cdad3b9815ef9e488d825
Alina SH <157328243+MistyHaven@users.noreply.github.com>
2025-08-20 14:40:07 -0400

    Update MLP_and_CNN_training.py
    Now trains 5 different MLPs and 5 different CNNS, as well as generates an html file containing inaccurately predicted images for each one. The code also creates an HTML file for the common inacurate images between all the MLPs/CNNs (separately)

--- MLP_and_CNN_training.py
+++ MLP_and_CNN_training.py
@@ -47,94 +47,129 @@ def download_img(folder_name):
 
 "TRAINING CNN AND MLP"
 
-def train_MLP(x_train, y_train_cat, x_test, y_test_cat):
+
+
+def mlp_builder(unit_1=128, unit_2=64):
+   model = Sequential([
+    Flatten(input_shape=(28,28)),
+    Dense(unit_1, activation="relu"),
+    Dense(unit_2, activation="relu"),
+    Dense(10, activation="softmax") # for digits 0-9
+    ])
+   
+   return model
+   
+
+
+
+def train_MLP(mlp_models, x_train, y_train_cat, x_test, y_test_cat):
    
    print("Training MLP start!")
-   model = Sequential([
+
+
+   '''model = Sequential([
     Flatten(input_shape=(28,28)),
     Dense(128, activation="relu"),
     #Dense(128, activation="relu", input_shape=(784,)),
     Dense(64, activation="relu"),
     Dense(10, activation="softmax") # for digits 0-9
-    ])
+    ])'''
    
-   model.compile(optimizer = 'adam', 
+   total_history = []
+   total_inaccurate_img = []
+   
+   
+   for model in mlp_models:
+      
+      model.compile(optimizer = 'adam', 
                  loss = "categorical_crossentropy", 
                  metrics = ['accuracy'])
    
-   history = model.fit(x_train, y_train_cat, epochs=10, batch_size=16, validation_split=0.1)
-   print("Training for MLP is completed!")
+      history = model.fit(x_train, y_train_cat, epochs=10, batch_size=16, validation_split=0.1)
+      print("Training for MLP is completed!")
 
-   metrics = model.evaluate(x_test, y_test_cat)
-   print("Results:")
-   for name, value in zip(model.metrics_names, metrics):
-      print(name,":",value)
+      metrics = model.evaluate(x_test, y_test_cat)
+      print("Results:")
+      for name, value in zip(model.metrics_names, metrics):
+         print(name,":",value)
 
-   y_pred = model.predict(x_test)
-   y_true = np.argmax(y_test_cat, axis = 1)
-   y_pred_labels = np.argmax(y_pred, axis = 1)
-
-
+      y_pred = model.predict(x_test)
+      y_true = np.argmax(y_test_cat, axis = 1)
+      y_pred_labels = np.argmax(y_pred, axis = 1)
 
 # Collect all the incorrectly predicted images
-   incorrect_pred = []
-   for i in range(len(y_test_cat)):
-      true_val = y_true[i]
-      if true_val!=y_pred_labels[i]:
+      incorrect_pred = []
+      for i in range(len(y_test_cat)):
+         true_val = y_true[i]
+         if true_val!=y_pred_labels[i]:
          #put the number of image in the list
-         incorrect_img_index = i 
-         incorrect_pred.append(incorrect_img_index)
+            incorrect_img_index = i 
+            incorrect_pred.append(incorrect_img_index)
    
    # Prepare dictionary will all needed values for the html file
-   inaccurate_imgs_for_html = []
-   for i in range(len(incorrect_pred)):
-      real_label = y_true[incorrect_pred[i]]
-      img_url = "MLP_and_CNN_training/"+str(real_label)+"/"+str(incorrect_pred[i]+60000)+".png"
-      #img_url = "MLP_and_CNN_training/MLP_and_CNN_training/"+str(real_label)+"/"+str(incorrect_pred[i]+60000)+".png"
-      #img_url = "/home/alina/MLP_and_CNN_training/MLP_and_CNN_training/"+str(real_label)+"/"+str(incorrect_pred[i]+60000)+".png"
-      pred_label = y_pred_labels[incorrect_pred[i]]
-      pred_perct = np.max(y_pred[incorrect_pred[i]])
-      img_info = {"img_url":img_url, "real_label": real_label, "predicted_label": pred_label, "confidence":pred_perct}
-      inaccurate_imgs_for_html.append(img_info)
+      inaccurate_imgs_for_html = []
+      for i in range(len(incorrect_pred)):
+         real_label = y_true[incorrect_pred[i]]
+         img_url = "MLP_and_CNN_training/"+str(real_label)+"/"+str(incorrect_pred[i]+60000)+".png"
+         pred_label = y_pred_labels[incorrect_pred[i]]
+         pred_perct = np.max(y_pred[incorrect_pred[i]])
+         img_info = {"img_url":img_url, "real_label": real_label, "predicted_label": pred_label, "confidence":pred_perct}
+         inaccurate_imgs_for_html.append(img_info)
    
-      
-   #Verifying if the image accuracy checker is working
-      '''#print("images", y_test_cat[i])
-      print("Incorrect image:", incorrect_pred[i]+60000)
-      #print("Real val:",y_test_cat[incorrect_pred[i]])
-      print("Real val:",np.argmax(y_test_cat[incorrect_pred[i]]))
-      #print("Predicted vals",y_pred_labels[index_incorrect])
-      #print("Predictions:",y_pred[incorrect_pred[i]])
-      print("Prediction perct:",np.max(y_pred[incorrect_pred[i]]))
-      print("Prediction val:",np.argmax(y_pred[incorrect_pred[i]]))
-      #print("Predicted vals",y_pred_labels[incorrect_pred[i]])
-      '''
-
    
    #print("MLP Classification Report:")
    #print(classification_report(y_true, y_pred_labels))
 
-   model_path = "MLP_and_CNN_training/mlp_model.h5"
-   if not os.path.exists(model_path):  
+      #model_results = {"history":history, "incorrect_img": inaccurate_imgs_for_html}
+      total_history.append(history)
+      total_inaccurate_img.append(inaccurate_imgs_for_html)
+      print(total_inaccurate_img[0][0])
+
+      model_path = "MLP_and_CNN_training/mlp_model"+str(mlp_models.index(model))+".h5"
+      if not os.path.exists(model_path):  
         model.save(model_path)
+   
+   ############# find intersection of all lists 
+   # list with list with many dictionarries with img
+
+   '''set1 = set(total_inaccurate_img[0]["img_url"])
+   set2 = set(total_inaccurate_img[1]["img_url"])
+   set3 = set(total_inaccurate_img[2]["img_url"])
+   set4 = set(total_inaccurate_img[3]["img_url"])
+   set5 = set(total_inaccurate_img[4]["img_url"])'''
+   
+   sets = [{dic["img_url"] for dic in inner_list} for inner_list in total_inaccurate_img]
+   intersection_img = set.intersection(*sets)
+   intersection_img = list(intersection_img)
+   #total_inaccurate_img.append(intersection_img)
     
-   return history, inaccurate_imgs_for_html
+   return total_history, total_inaccurate_img, intersection_img
 
 
 
-'''def inaccurate_img_urls(incorrect_pred, y_test_cat, y_pred):
-   # incorrect_pred - contains all the incorrect images, but you'll need to add 60000
-   #Will access the images from the folder
-   #makes table with image, real label, predicdted label and confidence level
-   total_inaccurate_img_url = []
-   for i in incorrect_pred:
-      img_url = str(incorrect_pred[i]+60000)+".png"
-      total_inaccurate_img_url.append(img_url)
-   return total_inaccurate_img_url
-'''
-def train_CNN(x_train_cnn, y_train_cat, x_test_cnn, y_test_cat):
-    print("Training CNN start!")
-    model = Sequential([
+
+def cnn_builder(filter_1=32, filter_2=64, dense_units=128, dropout = 0.5, kernel=(3,3)):
+   
+   model = Sequential([
+        Conv2D(filter_1, kernel, activation='relu', input_shape=(28,28, 1)), 
+        MaxPooling2D(pool_size=(2,2)),
+        Conv2D(filter_2, kernel, activation = "relu"),
+        MaxPooling2D(pool_size=(2,2)),
+        Flatten(),
+        Dense(dense_units, activation="relu"),
+        Dropout(dropout),
+        Dense(10, activation='softmax')
+        ])
+   
+   return model
+
+
+def train_CNN(cnn_models, x_train_cnn, y_train_cat, x_test_cnn, y_test_cat):
+    
+    
+   print("Training CNN start!")
+
+   '''model = Sequential([
         Conv2D(32, (3,3), activation='relu', input_shape=(28,28, 1)), 
         MaxPooling2D(pool_size=(2,2)),
         Conv2D(64, (3,3), activation = "relu"),
@@ -143,61 +178,133 @@ def train_CNN(x_train_cnn, y_train_cat, x_test_cnn, y_test_cat):
         Dense(128, activation="relu"),
         Dropout(0.5),
         Dense(10, activation='softmax')
-        ])
+        ])'''
     
-    model.compile(optimizer = "adam",
+   total_history = []
+   total_inaccurate_img = []
+
+   for model in cnn_models:
+      model.compile(optimizer = "adam",
                   loss = "categorical_crossentropy",
                   metrics = ['accuracy'])
     
-    history = model.fit(x_train_cnn, y_train_cat, epochs=10, batch_size=16, validation_split=0.1)
-    print("Training for CNN is completed!")
+      history = model.fit(x_train_cnn, y_train_cat, epochs=10, batch_size=16, validation_split=0.1)
+      print("Training for CNN is completed!")
+ 
+      metrics = model.evaluate(x_test_cnn, y_test_cat)
+      print("Results:")
+      for name, value in zip(model.metrics_names, metrics):
+         print(name,":",value)
 
-    metrics = model.evaluate(x_test_cnn, y_test_cat)
-    print("Results:")
-    for name, value in zip(model.metrics_names, metrics):
-       print(name,":",value)
+      y_pred = model.predict(x_test_cnn)
+      y_true = np.argmax(y_test_cat, axis = 1)
+      y_pred_labels = np.argmax(y_pred, axis = 1)
 
-    y_pred = model.predict(x_test_cnn)
-    y_true = np.argmax(y_test_cat, axis = 1)
-    y_pred_labels = np.argmax(y_pred, axis = 1)
-
-    #print("CNN Classification Report:")
+       #print("CNN Classification Report:")
     #print(classification_report(y_true, y_pred_labels))
 
     # Collect all the incorrectly predicted images
-    incorrect_pred = []
-    for i in range(len(y_test_cat)):
+      incorrect_pred = []
+      for i in range(len(y_test_cat)):
       #true_val = np.argmax(y_test_cat[i])
-      true_val = y_true[i]
-      if true_val!=y_pred_labels[i]:
+         true_val = y_true[i]
+         if true_val!=y_pred_labels[i]:
          #put the number of image in the list
-         incorrect_img_index = i 
-         incorrect_pred.append(incorrect_img_index)
+            incorrect_img_index = i 
+            incorrect_pred.append(incorrect_img_index)
    
    # Prepare dictionary will all needed values for the html file
-    inaccurate_imgs_for_html = []
-    for i in range(len(incorrect_pred)):
-      real_label = y_true[incorrect_pred[i]]
-      #MLP_and_CNN_training/MLP_and_CNN_training/9/4.pngs
-      #img_url = "MLP_and_CNN_training/MLP_and_CNN_training/"+str(real_label)+"/"+str(incorrect_pred[i]+60000)+".png"
-      img_url = "MLP_and_CNN_training/"+str(real_label)+"/"+str(incorrect_pred[i]+60000)+".png"
-      #img_url = "/home/alina/MLP_and_CNN_training/MLP_and_CNN_training/"+str(real_label)+"/"+str(incorrect_pred[i]+60000)+".png"
-      pred_label = y_pred_labels[incorrect_pred[i]]
-      pred_perct = np.max(y_pred[incorrect_pred[i]])
-      img_info = {"img_url":img_url, "real_label": real_label, "predicted_label": pred_label, "confidence":pred_perct}
-      inaccurate_imgs_for_html.append(img_info)
+      inaccurate_imgs_for_html = []
+      for i in range(len(incorrect_pred)):
+         real_label = y_true[incorrect_pred[i]]
+         #MLP_and_CNN_training/MLP_and_CNN_training/9/4.pngs
+         #img_url = "MLP_and_CNN_training/MLP_and_CNN_training/"+str(real_label)+"/"+str(incorrect_pred[i]+60000)+".png"
+         img_url = "MLP_and_CNN_training/"+str(real_label)+"/"+str(incorrect_pred[i]+60000)+".png"
+         #img_url = "/home/alina/MLP_and_CNN_training/MLP_and_CNN_training/"+str(real_label)+"/"+str(incorrect_pred[i]+60000)+".png"
+         pred_label = y_pred_labels[incorrect_pred[i]]
+         pred_perct = np.max(y_pred[incorrect_pred[i]])
+         img_info = {"img_url":img_url, "real_label": real_label, "predicted_label": pred_label, "confidence":pred_perct}
+         inaccurate_imgs_for_html.append(img_info)
+     
+      total_history.append(history)
+      total_inaccurate_img.append(inaccurate_imgs_for_html)
+     
+      model_path = "MLP_and_CNN_training/cnn_model"+str(cnn_models.index(model))+".h5"
+      model.save(model_path)  
 
-    model_path = "MLP_and_CNN_training/cnn_model.h5"
-    model.save(model_path)  
+   ############# find intersection of all lists
+   '''set1 = set(total_inaccurate_img[0]["img_url"])
+   set2 = set(total_inaccurate_img[1]["img_url"])
+   set3 = set(total_inaccurate_img[2]["img_url"])
+   set4 = set(total_inaccurate_img[3]["img_url"])
+   set5 = set(total_inaccurate_img[4]["img_url"])
+   intersection_img = list(set1 & set2 & set3 & set4 & set5)'''
+   
+   sets = [{dic["img_url"] for dic in inner_list} for inner_list in total_inaccurate_img]
+   intersection_img = set.intersection(*sets)
+   intersection_img = list(intersection_img)
+   #total_inaccurate_img.append(intersection_img)
+   
    #MLP_and_CNN_training/MLP_and_CNN_training/9/4.png
-    return history, inaccurate_imgs_for_html
+   return total_history, total_inaccurate_img, intersection_img
 
+ 
+
+def html_builder_intersection(intersection_img, model_name):
+   html_content ="""
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <meta charset="UTF-8">
+        <title> Image table </title>
+        <style>
+            table {
+               border-collapse: collapse;
+            }
+            td {
+               border: 1px solid black;
+               padding: 10px;
+               text-align: center;
+            }
+            img {
+               width: 50px;
+               height: auto;
+            }
+        </style>
+    </head>
+    <body>
+      <h2>Inaccurately Predicted Images</h2>
+      <table>
+         <tr>
+            <th> Image </th>
+         </tr>
+    """
+   for i in intersection_img: # access the dictionnaries in the inner lists
+      html_content += f"""
+      <tr>
+         <td><img src = "{i}" alt = "inaccurate img" width = "50" loading="lazy" ></td>
+      </tr>
+      """
+
+   html_content += """
+   </table>
+   </body>
+   </html>
+   """
+
+   html_name = model_name+"global_inaccurate_img_table.html"
+   with open(html_name, "w", encoding = "utf-8") as f:
+      f.write(html_content)
+
+   print("HTML table created: ", html_name)
+   
 
 #records all the images that cause the accuracy to drop, alongside their truth/predicted labels and confidence levels
-def html_builder(inaccurate_imgs_for_html, model_name):
+def html_builder(total_img_for_html, model_name):
    # will access each dictionary and populate the table
    # 
-   #   
+   # 
+ for list_num, inner_list in enumerate(total_img_for_html):  
    html_content ="""
     <!DOCTYPE html>
     <html>
@@ -229,7 +336,7 @@ def html_builder(inaccurate_imgs_for_html, model_name):
             <th> Confidence </th>
          </tr>
     """
-   for i in inaccurate_imgs_for_html:
+   for i in inner_list: # access the dictionnaries in the inner lists
       html_content += f"""
       <tr>
          <td><img src = "{i['img_url']}" alt = "inaccurate img" width = "50" loading="lazy" ></td>
@@ -238,12 +345,14 @@ def html_builder(inaccurate_imgs_for_html, model_name):
          <td>{i['confidence']:.2f}</td>
       </tr>
       """
+
    html_content += """
    </table>
    </body>
    </html>
    """
-   html_name = model_name+"_inaccurate_img_table.html"
+
+   html_name = model_name+"_inaccurate_img_table"+ str(list_num)+".html"
    with open(html_name, "w", encoding = "utf-8") as f:
       f.write(html_content)
 
@@ -343,7 +452,28 @@ if __name__== '__main__':
 
     title_cnn = "CNN"
     save_path_cnn = "cnn_graph.png"
-    #print("your mom")
+    
+
+   # generate our 5 different mlps:
+
+    mlp_models = [
+      mlp_builder(128, 64),
+      mlp_builder(256, 128),
+      mlp_builder(64, 32),
+      mlp_builder(512, 256),
+      mlp_builder(128, 128)
+   ]
+    
+    cnn_models = [
+       cnn_builder(32,64,128,0.5,(3,3)), # baseline
+       cnn_builder(64,128,256,0.5,(3,3)), # bigger filters
+       cnn_builder(32,64,128,0.3,(3,3)), # lower dropout
+       cnn_builder(32,64,128,0.5,(5,5)), # bigger kernel
+       cnn_builder(16,32,128,0.5,(3,3)) # smaller filters
+    ]
+
+
+
 
     
     #download_img(folder_name)
@@ -351,24 +481,28 @@ if __name__== '__main__':
     #train MLP
     #print("MEOW2")
     #history_mlp = train_MLP(x_train_mlp, y_train_cat, x_test_mlp, y_test_cat)
-    history_mlp, inaccurate_imgs_mlp = train_MLP(x_train, y_train_cat, x_test, y_test_cat)
+    total_history_mlp, total_inaccurate_img_mlp, intersection_img_mlp = train_MLP(mlp_models, x_train, y_train_cat, x_test, y_test_cat)
     #plot_learning_curve(history_mlp, title_mlp, save_path_mlp)
-    print(inaccurate_imgs_mlp[0])
+
+
     tf.keras.backend.clear_session()
     
-    #print("MEOW3")
     #train CNN
-    history_cnn, inaccurate_imgs_cnn = train_CNN(x_train_cnn, y_train_cat, x_test_cnn, y_test_cat)  
+    total_history_cnn, total_inaccurate_imgs_cnn, intersection_img_cnn = train_CNN(cnn_models, x_train_cnn, y_train_cat, x_test_cnn, y_test_cat)  
     #plot_learning_curve(history_cnn, title_cnn, save_path_cnn)
+
+
 
 
    #build our html file
 
    #For MLP
-    html_builder(inaccurate_imgs_mlp, title_mlp)
+    html_builder(total_inaccurate_img_mlp, title_mlp)
+    html_builder_intersection(intersection_img_mlp, title_mlp)
 
    #For CNN
-    html_builder(inaccurate_imgs_cnn, title_cnn)
+    html_builder(total_inaccurate_imgs_cnn, title_cnn)
+    html_builder_intersection(intersection_img_cnn, title_cnn)
 
     
     
